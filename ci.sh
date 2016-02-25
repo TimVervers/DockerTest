aaa3a5b5a90fd3a39245368c8946dfe26f91cb8b
Tim Ververs <timververs@msn.com>
2016-02-25 17:00:15 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,1 +1,16 @@
-echo "Done"
+echo "Step 1 : Composer"
+
+echo "Install Composer"
+
+#Install Composer
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+echo "Run Composer"
+
+#Actual running
+echo "Composing..."
+
+echo "Done with composing."
