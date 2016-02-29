3d99eded01303e81bd4204308004a6752c6d562b
Tim Ververs <timververs@msn.com>
2016-02-29 14:23:22 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,9 +1,11 @@
+echo "Install composer in project"
+composer install
+
 echo "Get PHP Codesniffer"
 composer global require "squizlabs/php_codesniffer=*"
 
-echo "Install composer in project"
-composer update
-# temp update for codesnifffer
-
 echo "Run PHP lint"
 find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
+
+echo "Run code sniffer"
+./vendor/bin/phpcs -h
