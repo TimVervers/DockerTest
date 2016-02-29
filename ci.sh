32c39bf07df220366d844e280f11c879f908600a
Tim Ververs <timververs@msn.com>
2016-02-29 15:41:38 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,7 +1,5 @@
 echo "Run PHP lint"
 find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
 
-sleep 5h
-
 echo "Run code sniffer"
-./vendor/squizlabs/php_codesniffer/scripts/phpcs -h
+./vendor/bin/phpcs .
