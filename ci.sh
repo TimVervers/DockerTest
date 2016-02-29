1b99934f2bab3ecf3e498e2ed76e0cc365a8a07d
Tim Ververs <timververs@msn.com>
2016-02-29 16:28:35 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@ echo "Run PHP lint"
 find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
 
 #echo "Run code sniffer"
-# ./vendor/bin/phpcs ./src ./tests
+./vendor/bin/phpcs ./src ./tests
 
 echo "Run PHPUnit"
 ./vendor/bin/phpunit --configuration phpunit.xml
