e9fc8eb65e30a8d8b1baa203dd34e021af4cf713
Tim Ververs <timververs@msn.com>
2016-02-29 11:57:05 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -6,11 +6,11 @@ apt-get -y install unzip
 echo "Download composer"
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
 php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
-php composer-setup.php
+php composer-setup.php --install-dir=bin --filename=composer
 php -r "unlink('composer-setup.php');"
 
 echo "Run Composer"
-php composer.phar update
+php composer.phar install
 
 echo "Run PHP lint"
 find -L . -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
