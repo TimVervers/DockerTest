1e8e688a351f2c121c872c5160f5f3750aed4e1d
Tim Ververs <timververs@msn.com>
2016-02-29 12:37:06 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,5 +1,8 @@
-echo "Run Composer"
-php composer.phar install
+echo "Check if composer is installed""
+composer
+
+ech "Update composer"
+composer update
 
 echo "Run PHP lint"
 find -L . -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
