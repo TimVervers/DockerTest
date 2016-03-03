895110a1d1024c67981f088f3d8a95e4f90e0090
Tim Ververs <timververs@msn.com>
2016-03-03 12:07:13 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -7,5 +7,9 @@ find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 ph
 # Run PHPUnit
 ./vendor/bin/phpunit --configuration phpunit.xml
 
+# Create artifact
+rm -f /share/artifacts/*.txt
+cat /proc/self/cgroup | grep 'docker' | sed 's/^.*\///' | tail -n1 > /share/artifacts/docker_containerId.txt
+
 # Go into sleep mode
 # sleep 5h
