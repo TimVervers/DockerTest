2551e60a7c6a47fc9dfc9ee60113c384433ea720
Tim Ververs <timververs@msn.com>
2016-03-14 13:47:35 +0100

    Update initial.sh

--- .NET/initial.sh
+++ .NET/initial.sh
@@ -1,5 +1,6 @@
 #restore all packages
 dnu restore
 
-#run program
-dnx -p project.json kestrel
+echo "We did some stuff, now just stop the container... It proves we could do stuff inside here. Let's create an artifact tho".
+
+echo "My name is Arti, Arti Fact. Want to hear a nice fact? Some birds around Chernobyl appear to be evolving to suit the radioactive environment." > ArtiFact.txt
