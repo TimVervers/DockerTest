baa0ea191682318d44c219c769e29158908cf9c0
Tim Ververs <timververs@msn.com>
2016-03-16 15:52:07 +0100

    Update ci.sh

--- PHP/ci.sh
+++ PHP/ci.sh
@@ -4,11 +4,8 @@ curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
 # Run PHPLint
 find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
 
-# Run code sniffer
-./vendor/bin/phpcs ./src ./tests
-
 # Run PHPUnit
-./vendor/bin/phpunit --configuration phpunit.xml
+./vendor/bin/phpunit --configuration phpunit.xml.dist
 
 # Create artifact
 cat /proc/self/cgroup | grep 'docker' | sed 's/^.*\///' | tail -n1 > docker_containerId.txt
