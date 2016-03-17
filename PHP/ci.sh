e4f25268eec83aae3db249fcf5c55fe462ee69c7
Tim Ververs <timververs@msn.com>
2016-03-17 11:27:09 +0100

    Update ci.sh

--- PHP/ci.sh
+++ PHP/ci.sh
@@ -5,4 +5,4 @@ find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 ph
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && composer install --prefer-source --no-interaction --no-progress
 
 # Run PHPUnit
-vendor/bin/phpunit --coverage-clover=phpunit-clover.xml --coverage-html=phpunit-coverage --log-junit=phpunit-junit.xml
+vendor/bin/phpunit --coverage-clover=./coverage/phpunit-clover.xml --coverage-html=phpunit-coverage --log-junit=./coverage/phpunit-junit.xml
