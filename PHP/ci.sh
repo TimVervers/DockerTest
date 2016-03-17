43a9d0a1ce5999fdba040b9f0a2a9e12763ecb50
Tim Ververs <timververs@msn.com>
2016-03-17 10:50:18 +0100

    Update ci.sh

--- PHP/ci.sh
+++ PHP/ci.sh
@@ -1,11 +1,8 @@
-#Install and run composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && composer install --prefer-source --no-interaction
-
 # Run PHPLint
 find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
 
-# Run PHPUnit
-./vendor/bin/phpunit --configuration phpunit.xml.dist
+#Install and run composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && composer install --prefer-source --no-interaction --no-progress
 
-# Create artifact
-cat /proc/self/cgroup | grep 'docker' | sed 's/^.*\///' | tail -n1 > docker_containerId.txt
+# Run PHPUnit
+composer test
