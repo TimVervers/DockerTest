a18bc56ab331a250f4bd58b5c21fe1a3cd3c6002
Tim Ververs <timververs@msn.com>
2016-03-30 12:10:49 +0200

    Update ci.sh

--- PHP/ci.sh
+++ PHP/ci.sh
@@ -4,6 +4,9 @@ find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 ph
 #Run composer
 composer install --prefer-source --no-interaction --no-progress
 
+#Set compsoser timeout
+export COMPOSER_PROCESS_TIMEOUT=2000
+
 # Run PHPUnit
 composer test -vvv
 #vendor/bin/phpunit --coverage-clover=./coverage/phpunit-clover.xml --coverage-html=phpunit-coverage --log-junit=./coverage/phpunit-junit.xml
