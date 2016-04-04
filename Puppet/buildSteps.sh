6b888a71ccfd24d05319fcbe4281f6026e5b41b7
Tim Ververs <timververs@msn.com>
2016-04-04 14:30:48 +0200

    Create buildSteps.sh

---
+++ Puppet/buildSteps.sh
@@ -0,0 +1,13 @@
+# Run bundle install
+bundle install --binstubs --path vendor/bundle 
+
+# Run librarian install
+bundle exec rake librarian:install
+
+# Run tests
+bundle exec rake spec_clean
+bundle exec rake spec_prep
+bundle exec rake spec_standalone
+
+# Run puppet lint
+bundle exec rake lint:puppet
