41587903e8733da49f76457fe215824c12c668c3
Tim Ververs <TimVervers@users.noreply.github.com>
2016-04-06 16:56:17 +0200

    Update prepare.sh

--- Puppet/Prepare-Build/prepare.sh
+++ Puppet/Prepare-Build/prepare.sh
@@ -1,1 +1,15 @@
+# Run bundle install
+bundle install --binstubs --path vendor/bundle 
 
+# Run librarian install
+bundle exec rake librarian:clean_install
+
+# Run tests
+#<target name="version-number" description="Replaces the @BUILD_NUMBER@ variable with the TeamCity build number.">
+      #  <property file="${teamcity.build.checkoutDir}/build.properties"/>
+     #   <property name="spec.source.path" value="${teamcity.build.checkoutDir}/${spec.file.name}"/>
+    #    <property name="spec.destination.path" value="${teamcity.build.tempDir}/${spec.file.name}"/>
+   #     <copy file="${spec.source.path}" todir="${teamcity.build.tempDir}"/>
+  #      <replace file="${spec.destination.path}" token="@BUILD_NUMBER@" value="${build.number}"/>
+ #       <echo message="##teamcity[publishArtifacts '${spec.destination.path}']"/>
+#</target>
