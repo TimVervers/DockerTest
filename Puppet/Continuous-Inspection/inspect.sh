59d5f1276afb10e12941bda06f5c519457f4496c
Tim Ververs <TimVervers@users.noreply.github.com>
2016-04-06 13:24:17 +0200

    Create inspect.sh

---
+++ Puppet/Continuous-Inspection/inspect.sh
@@ -0,0 +1,12 @@
+# Run bundle install
+bundle install --binstubs --path vendor/bundle 
+
+# Run librarian install
+bundle exec rake librarian:clean_install
+
+# Run tests
+bundle exec rake spec_prep
+bundle exec rake spec_unit
+
+# Run puppet lint
+bundle exec rake lint:puppet
