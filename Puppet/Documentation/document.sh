aecbc1071304946c1102ad088351f067dea477c5
Tim Ververs <TimVervers@users.noreply.github.com>
2016-04-06 14:00:17 +0200

    Update document.sh

--- Puppet/Documentation/document.sh
+++ Puppet/Documentation/document.sh
@@ -1,1 +1,9 @@
+# Run bundle install
+bundle install --binstubs --path vendor/bundle 
 
+# Generate documentation
+bundle exec rake documentation:generate_ip
+
+# Publish documentation - DISABLED, i don't want to change to documentation from my local POC. 
+# Enable this if the POC gets into prod
+# bundle exec rake documentation:publish
