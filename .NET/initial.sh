c75daf05f7a17fd733a8d9e0cd4fb83c9291432a
Tim Ververs <t.ververs@coolblue.nl>
2016-06-15 15:42:37 +0200

    a

--- .NET/initial.sh
+++
@@ -1,6 +0,0 @@
-#restore all packages
-echo "My name is Arti, Arti Fact."
-echo "Want to hear a nice fact?."
-echo "Some birds around Chernobyl appear to be evolving to suit the radioactive environment."
-echo "Some birds around Chernobyl appear to be evolving to suit the radioactive environment." > ArtiFact.txt
-echo "I just saved that into an artifact! Have fun, byebye."
